53c4639f9edaa092f0b529047006488a77adfc0c
wmartyr <woodrow.martyr@gmail.com>
2022-10-25 22:05:51 +1030

    Create gameboard module

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,25 @@
 const gameboard = (() => {
-    const gameArray = [];
-    var clicked = () => {
+    const gameArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const clicked = (playerToken) => {
         const buttons = document.querySelectorAll(".tile");
         buttons.forEach((button) => {
             button.addEventListener("click", () => {
                 alert(button.id);
+                gameArray[button.id] = playerToken;
+                updateTile(playerToken, button);
             });
         });
     };
+
+    const updateTile = (player, button) => {
+        
+        if (player === "x") {
+            button.textContent = "X";
+        } else {
+            button.textContent = "O";
+        }
+    }
+
 
     return {
         clicked,
@@ -15,4 +27,4 @@ const gameboard = (() => {
 
 })();
 
-gameboard.clicked();
+gameboard.clicked("o");
