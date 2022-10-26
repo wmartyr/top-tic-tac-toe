945b8dccc442fa9146fa3a2df4b53277b8bf4296
wmartyr <woodrow.martyr@gmail.com>
2022-10-26 20:17:43 +1030

    Add gameStart & boardReset functions to gameboard

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 const gameboard = (() => {
     const gameArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const gameStart = (playerToken) => {
+        clicked(playerToken);
+    };
+
     const clicked = (playerToken) => {
         const buttons = document.querySelectorAll(".tile");
         buttons.forEach((button) => {
             button.addEventListener("click", () => {
-                alert(button.id);
                 gameArray[button.id] = playerToken;
                 updateTile(playerToken, button);
             });
@@ -12,19 +16,25 @@ const gameboard = (() => {
     };
 
     const updateTile = (player, button) => {
-        
         if (player === "x") {
             button.textContent = "X";
         } else {
             button.textContent = "O";
         }
-    }
+    };
 
+    const boardReset = () => {
+        gameArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const buttons = document.querySelectorAll(".tile");
+        buttons.forEach((button) => {
+            button.textContent = "";
+        });
+    };
 
     return {
-        clicked,
+        gameStart,
     };
 
 })();
 
-gameboard.clicked("o");
+gameboard.gameStart("o");
